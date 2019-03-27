71fea20fdffb4e881ff575fe73352e09d4677226
Akash Eldo <akasheldo@hotmail.com>
2019-03-27 19:06:17 -0400

    Modified PlayVideo for WebCam Object Tracking

--- Assets/Scripts/PlayVideo.cs
+++ Assets/Scripts/PlayVideo.cs
@@ -15,17 +15,19 @@ public class PlayVideo : MonoBehaviour {
     VideoCapture capture;
     Texture2D texture;
     TrackerCSRT tracker;
-
+    Rectangle handBox = new Rectangle(150, 150, 200, 200);
+    bool isReady = false;
     // Start is called before the first frame update
     void Start() {
-        capture = new VideoCapture("C:\\Users\\akash\\Downloads\\hand_vid.mp4");
+        capture = new VideoCapture(0);
         Image<Bgr, Byte> initialFrame = capture.QueryFrame().ToImage<Bgr,Byte>();
         tracker = new TrackerCSRT();
-        Image<Bgr,Byte> hand = GetSkin(initialFrame);
-        Rectangle handBox = DoCascadeRect(hand);
+        //Image<Bgr,Byte> hand = GetSkin(initialFrame);
+        //Rectangle handBox = DoCascadeRect(hand);
 
-        tracker.Init(initialFrame.Mat, handBox);
+        //tracker.Init(initialFrame.Mat, handBox);
 
+        //initialFrame.Draw(handBox, new Bgr(System.Drawing.Color.Green), 3);
         initialFrame.Draw(handBox, new Bgr(System.Drawing.Color.Green), 3);
         texture = TextureConvert.ImageToTexture2D<Bgr, Byte>(initialFrame, FlipType.None);
         GetComponent<Renderer>().material.mainTexture = texture;
@@ -34,12 +36,22 @@ public class PlayVideo : MonoBehaviour {
     // Update is called once per frame
     void Update() {
         Image<Bgr, Byte> frame = capture.QueryFrame().ToImage<Bgr,Byte>();
-        Rectangle box;
-        tracker.Update(frame.Mat, out box);
-        if(box != null){
-            frame.Draw(box, new Bgr(System.Drawing.Color.Green), 3);
-        }else {
-            Debug.Log("Box is null");
+        if (Input.GetKeyDown("space") && !isReady) {
+            print("space key was pressed");
+            tracker.Init(frame.Mat, handBox);
+            isReady = true;
+        }
+
+        if (isReady) {
+            Rectangle box;
+            tracker.Update(frame.Mat, out box);
+            if (box != null) {
+                frame.Draw(box, new Bgr(System.Drawing.Color.Green), 3);
+            } else {
+                Debug.Log("Box is null");
+            }
+        } else {
+            frame.Draw(handBox, new Bgr(System.Drawing.Color.Green), 3);
         }
         GetComponent<Renderer>().material.mainTexture = TextureConvert.ImageToTexture2D<Bgr, byte>(frame,FlipType.None);
     }
