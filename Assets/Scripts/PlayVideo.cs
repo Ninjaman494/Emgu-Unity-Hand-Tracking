8be850d9d3d28438e7999b18bb5ee3b3fb31ccad
Akash Eldo <akasheldo@hotmail.com>
2019-03-27 20:45:09 -0400

    Now spawns cube in initialization frame PlayVideo converts xy coord of the rectangle to screen coords for spawning Created a function handle this conversion

--- Assets/Scripts/PlayVideo.cs
+++ Assets/Scripts/PlayVideo.cs
@@ -12,25 +12,35 @@ using System.Drawing;
 using static SkinDetection;
 
 public class PlayVideo : MonoBehaviour {
+
+    public Camera camera;
+
     VideoCapture capture;
     Texture2D texture;
     TrackerCSRT tracker;
-    Rectangle handBox = new Rectangle(150, 150, 200, 200);
+    Rectangle handBox;// = new Rectangle(150, 150, 200, 200);
     bool isReady = false;
     // Start is called before the first frame update
     void Start() {
         capture = new VideoCapture(0);
         Image<Bgr, Byte> initialFrame = capture.QueryFrame().ToImage<Bgr,Byte>();
         tracker = new TrackerCSRT();
-        //Image<Bgr,Byte> hand = GetSkin(initialFrame);
-        //Rectangle handBox = DoCascadeRect(hand);
+        
+        // Drawing inital handBox
+        Vector3 size = GetComponent<Renderer>().bounds.size;
+        float width = size.x;
+        float height = size.y;
+        float widthX = width / initialFrame.Width;
+        float heightX = height / initialFrame.Height;
+        handBox = new Rectangle((int)(5/widthX), (int)(5/heightX), 1, 1);
 
-        //tracker.Init(initialFrame.Mat, handBox);
-
-        //initialFrame.Draw(handBox, new Bgr(System.Drawing.Color.Green), 3);
-        initialFrame.Draw(handBox, new Bgr(System.Drawing.Color.Green), 3);
+        initialFrame.Draw(handBox, new Bgr(System.Drawing.Color.Green), 2);
         texture = TextureConvert.ImageToTexture2D<Bgr, Byte>(initialFrame, FlipType.None);
         GetComponent<Renderer>().material.mainTexture = texture;
+
+        // Spawn "hand"
+        GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        cube.transform.position = VideoCoordToScreenCoord(handBox.X, handBox.Y, initialFrame.Width, initialFrame.Height);
     }
 
     // Update is called once per frame
@@ -54,5 +64,18 @@ public class PlayVideo : MonoBehaviour {
             frame.Draw(handBox, new Bgr(System.Drawing.Color.Green), 3);
         }
         GetComponent<Renderer>().material.mainTexture = TextureConvert.ImageToTexture2D<Bgr, byte>(frame,FlipType.None);
+    }
+
+    void OnApplicationQuit() {
+        capture.Dispose();
+    }
+
+    Vector3 VideoCoordToScreenCoord(float x, float y, float frameWidth, float frameHeight) {
+        float widthY = (float)camera.pixelWidth / frameWidth;
+        float heightY = (float)camera.pixelHeight / frameHeight;
+        GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        Vector3 vector = camera.ScreenToWorldPoint(new Vector3(x * widthY,y * heightY, 0));
+        vector.z = 0;
+        return vector;
     }
 }
